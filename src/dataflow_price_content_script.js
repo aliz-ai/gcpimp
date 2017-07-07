f125dac2d1aa0237bc71e31eb69291715203ff5b
Peter Farkas <peter.farkas@doctusoft.com>
2017-07-07 18:23:36 +0200

    Add properties to UI.

--- src/dataflow_price_content_script.js
+++ src/dataflow_price_content_script.js
@@ -1,27 +1,72 @@
 function parseMetric(metricString) {
 	return parseFloat(metricString.trim().split(' ')[0], 10)
 }
-var firstRowOfMetrics = document.querySelector('dax-service-metrics div.p6n-kv-list-item');
-var metrics = document.querySelectorAll('dax-service-metrics div.p6n-kv-list-value span span');
 
-console.log(metrics);
+var enhancePanel = function () {
+	var firstRowOfMetrics = document.querySelector('dax-service-metrics div.p6n-kv-list-item');
+	var metrics = document.querySelectorAll('dax-service-metrics div.p6n-kv-list-value span span');
 
-var currentCPU = parseMetric(metrics[0].innerHTML)
-var totalCPU = parseMetric(metrics[1].innerHTML)
+	// this should be 8 or 9 by default, so we'll only add the properties once
+	if (metrics.length <= 9) {
 
-var currentMemory = parseMetric(metrics[2].innerHTML)
-var totalMemory = parseMetric(metrics[3].innerHTML)
+		var currentCPU = parseMetric(metrics[0].innerHTML)
+		var totalCPU = parseMetric(metrics[1].innerHTML)
 
-var currentPD = parseMetric(metrics[4].innerHTML)
-var totalPD = parseMetric(metrics[5].innerHTML)
+		var currentMemory = parseMetric(metrics[2].innerHTML)
+		var totalMemory = parseMetric(metrics[3].innerHTML)
 
-var currentSSD = parseMetric(metrics[6].innerHTML)
-var totalSSD = parseMetric(metrics[7].innerHTML)
+		var currentPD = parseMetric(metrics[4].innerHTML)
+		var totalPD = parseMetric(metrics[5].innerHTML)
 
-var currentCostRow = firstRowOfMetrics.cloneNode(true);
-var totalCostRow = firstRowOfMetrics.cloneNode(true);
+		var currentSSD = parseMetric(metrics[6].innerHTML)
+		var totalSSD = parseMetric(metrics[7].innerHTML)
 
-currentCostRow.querySelector("div > span:first-child").innerHTML = " Current cost per hour ";
-currentCostRow.querySelector("span.p6n-help-tooltip-msg span").innerHTML = " The cost of running this Dataflow job for an hour. ";
+		var currentCostRow = firstRowOfMetrics.cloneNode(true);
+		var totalCostRow = firstRowOfMetrics.cloneNode(true);
 
-firstRowOfMetrics.parentNode.addChild(currentCostRow);
+		currentCostRow.querySelector("div.p6n-kv-list-key > span:first-child").innerHTML = " Current cost ";
+		currentCostRow.querySelector("div.p6n-kv-list-key > span:last-child").remove();
+		currentCostRow.querySelector("dax-default-value span span").innerHTML = "SADASDASD";
+
+		totalCostRow.querySelector("div.p6n-kv-list-key > span:first-child").innerHTML = " Total cost ";
+		totalCostRow.querySelector("div.p6n-kv-list-key > span:last-child").remove();
+
+		firstRowOfMetrics.parentNode.appendChild(currentCostRow);
+		firstRowOfMetrics.parentNode.appendChild(totalCostRow);
+	}
+};
+
+function waitFor(isPresent, callback) {
+	if (isPresent()) {
+		callback();
+		return;
+	}
+	var time = 0;
+	var key = setInterval(function () {
+		if (isPresent() || time > 10000) {
+			clearInterval(key);
+			if (isPresent()) {
+				callback();
+			}
+			return;
+		}
+		time += 1000;
+	}, 1000);
+}
+
+function waitAndEnhance() {
+	waitFor(() => {
+		var list = document.querySelectorAll('dax-service-metrics div.p6n-kv-list-value span span');
+		return list !== undefined && list.length >= 9;
+	}, enhancePanel);
+}
+
+window.addEventListener('load', () => {
+	waitAndEnhance();
+	waitFor(() => {
+		var body = document.querySelector(".p6n-dax-graph");
+		return body !== undefined && body !== null;
+	}, () => {
+		document.querySelector(".p6n-dax-graph").addEventListener('click', waitAndEnhance);
+	});
+});
