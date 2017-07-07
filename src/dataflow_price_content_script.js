65ccf85c3cca4a7d82a93fe3f910d2de21255e91
Csaba Kassai <csaba.kassai@doctusoft.com>
2017-07-07 16:13:44 +0200

    Button to dataflow

---
+++ src/dataflow_price_content_script.js
@@ -0,0 +1,6 @@
+//Creating Elements
+var btn = document.createElement("BUTTON")
+var t = document.createTextNode("HAHAHAHAHAHHAHAHAHAHA");
+btn.appendChild(t);
+//Appending to DOM
+document.body.appendChild(btn);
