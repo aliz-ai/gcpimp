c5ae0b90de9979dcfb5d0bb2b4796276a88b0e67
Peter Farkas <peter.farkas@doctusoft.com>
2017-07-07 17:15:12 +0200

    Try to add a new row

--- src/dataflow_price_content_script.js
+++ src/dataflow_price_content_script.js
@@ -1,21 +1,27 @@
-
 function parseMetric(metricString) {
 	return parseFloat(metricString.trim().split(' ')[0], 10)
 }
+var firstRowOfMetrics = document.querySelector('dax-service-metrics div.p6n-kv-list-item');
+var metrics = document.querySelectorAll('dax-service-metrics div.p6n-kv-list-value span span');
 
-val metrics = $$('dax-service-metrics div.p6n-kv-list-value span span');
+console.log(metrics);
 
-val currentCPU = parseMetric(metrics[0].innerText)
-val totalCPU = parseMetric(metrics[1].innerText)
+var currentCPU = parseMetric(metrics[0].innerHTML)
+var totalCPU = parseMetric(metrics[1].innerHTML)
 
-val currentMemory = parseMetric(metrics[2].innerText)
-val totalMemory = parseMetric(metrics[3].innerText)
+var currentMemory = parseMetric(metrics[2].innerHTML)
+var totalMemory = parseMetric(metrics[3].innerHTML)
 
-val currentPD = parseMetric(metrics[4].innerText)
-val totalPD = parseMetric(metrics[5].innerText)
+var currentPD = parseMetric(metrics[4].innerHTML)
+var totalPD = parseMetric(metrics[5].innerHTML)
 
-val currentSSD = parseMetric(metrics[6].innerText)
-val totalSSD = parseMetric(metrics[7].innerText)
+var currentSSD = parseMetric(metrics[6].innerHTML)
+var totalSSD = parseMetric(metrics[7].innerHTML)
 
+var currentCostRow = firstRowOfMetrics.cloneNode(true);
+var totalCostRow = firstRowOfMetrics.cloneNode(true);
 
+currentCostRow.querySelector("div > span:first-child").innerHTML = " Current cost per hour ";
+currentCostRow.querySelector("span.p6n-help-tooltip-msg span").innerHTML = " The cost of running this Dataflow job for an hour. ";
 
+firstRowOfMetrics.parentNode.addChild(currentCostRow);
