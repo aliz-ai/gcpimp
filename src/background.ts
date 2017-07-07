2fc3dcd68964a5ee7f08d93bce00f88511f01cf5
Csaba Kassai <csaba.kassai@doctusoft.com>
2017-07-08 00:32:17 +0200

    Bugfix

--- src/background.ts
+++ src/background.ts
@@ -1,9 +1,5 @@
-
-chrome.tabs.onUpdated.addListener(
-  function(tabId, changeInfo, tab) {
-      console.log(changeInfo.url);
-      if(changeInfo.url.search('https://console.cloud.google.com/dataflow/job/') >= 0) {
-          chrome.tabs.executeScript(null, {file: "dataflow_price_content_script.js"});
-      }
-  }
-);
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (tab.url.search('https://console.cloud.google.com/dataflow/job/') >= 0) {
+        chrome.tabs.executeScript(null, { file: "dataflow_price_content_script.js" });
+    }
+});
