25e720f7eb8d182012563ecf9ca2a79aa3c4dd71
Csaba Kassai <csaba.kassai@doctusoft.com>
2017-07-07 16:44:16 +0200

    Parsing dataflow metrics

--- src/dataflow_price_content_script.js
+++ src/dataflow_price_content_script.js
@@ -1,6 +1,21 @@
-//Creating Elements
-var btn = document.createElement("BUTTON")
-var t = document.createTextNode("HAHAHAHAHAHHAHAHAHAHA");
-btn.appendChild(t);
-//Appending to DOM
-document.body.appendChild(btn);
+
+function parseMetric(metricString) {
+	return parseFloat(metricString.trim().split(' ')[0], 10)
+}
+
+val metrics = $$('dax-service-metrics div.p6n-kv-list-value span span');
+
+val currentCPU = parseMetric(metrics[0].innerText)
+val totalCPU = parseMetric(metrics[1].innerText)
+
+val currentMemory = parseMetric(metrics[2].innerText)
+val totalMemory = parseMetric(metrics[3].innerText)
+
+val currentPD = parseMetric(metrics[4].innerText)
+val totalPD = parseMetric(metrics[5].innerText)
+
+val currentSSD = parseMetric(metrics[6].innerText)
+val totalSSD = parseMetric(metrics[7].innerText)
+
+
+
