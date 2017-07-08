83eb22c963827362964cd45fb588202a6017a49a
Peter Farkas <peter.farkas@doctusoft.com>
2017-07-08 02:30:46 +0200

    AppEngine log URL

---
+++ src/appengine_log_script.ts
@@ -0,0 +1,62 @@
+//document.getElementsByTagName("header")[0].style.background = "lightblue";
+
+document.addEventListener("click", function() { 
+	var elements = document.getElementsByClassName("p6n-logs-json-key");
+	if (elements.length == 0) {
+		return;
+	}
+
+	var requestIdNodes = getLogElementsByName(elements, "requestId: ");
+	for (i = 0; i < requestIdNodes.length; i++) {
+		var requestIdNode = requestIdNodes[i];
+		var requestIdParentNode = requestIdNode.parentNode;
+
+		if (requestIdParentNode.getElementsByTagName("a").length != 0) {
+			continue;
+		}
+
+		var a = document.createElement('a');
+		var linkText = document.createTextNode("Link to here");
+		a.appendChild(linkText);
+		a.title = "copy";
+		a.style = "margin-left: 10px;";
+		requestIdParentNode.appendChild(a);
+
+		var requestIdValue = requestIdParentNode.getElementsByClassName("p6n-logs-json-value")[0].innerText;
+		requestIdValue = requestIdValue.substring(1, requestIdValue.length - 1);
+		console.log(requestIdValue);
+
+		console.log(window.location.href);
+
+		var url = window.location.href;
+		url = updateQueryStringParameter(url, "filters", "protoPayload.requestId:" + requestIdValue);
+		console.log(url);
+
+		var endTime = getLogElementsByName(requestIdParentNode.parentNode.parentNode.getElementsByClassName("p6n-logs-json-key"), "endTime: ")[0].parentNode.getElementsByClassName("p6n-logs-json-value")[0].innerText;
+		endTime = endTime.substring(1, endTime.length - 2);
+		endTime += "000Z";
+
+		url = updateQueryStringParameter(url, "timestamp", endTime);
+
+		a.href = url;
+	}
+});
+
+function getLogElementsByName(elements, name) {
+	return Array.prototype.filter.call(elements, function(testElement) {
+    		return testElement.innerText === name;
+	});
+}
+
+
+function updateQueryStringParameter(uri, key, value) {
+	var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
+	var separator = uri.indexOf('?') !== -1 ? "&" : "?";
+ 	if (uri.match(re)) {
+    		return uri.replace(re, '$1' + key + "=" + value + '$2');
+	}
+	else {
+    		return uri + separator + key + "=" + value;
+  	}
+}
+
--- src/background.ts
+++ src/background.ts
@@ -1,6 +1,9 @@
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     if (tab.url.search('https://console.cloud.google.com/dataflow/job/') >= 0) {
-        chrome.tabs.executeScript(null, { file: "dataflow_price_content_script.js" });
+        chrome.tabs.executeScript(tabId, { file: "dataflow_price_content_script.js" });
+    }
+    if (tab.url.search('https://console.cloud.google.com/logs/viewer') >= 0) {
+        chrome.tabs.executeScript(tabId, { file: 'appengine_log_script.js' });
     }
 });
 
@@ -21,8 +24,3 @@ class AuthToken {
         });
     }
 }
-
-chrome.identity.getAuthToken({
-    interactive: true,
-    scopes: ['https://www.googleapis.com/auth/bigquery', 'https://www.googleapis.com/auth/devstorage.full_control']
-}, console.log);
