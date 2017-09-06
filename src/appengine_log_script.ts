25077aa8f2bcbb25bcaad387e21f322851577b3e
Gergely Sipos <gergely.sipos@doctusoft.com>
2017-09-06 20:15:39 +0200

    refactor, tslint config

--- src/appengine_log_script.ts
+++ src/appengine_log_script.ts
@@ -1,58 +1,52 @@
-//document.getElementsByTagName("header")[0].style.background = "lightblue";
+// document.getElementsByTagName("header")[0].style.background = "lightblue";
 
-document.addEventListener("click", () => {
-	var elements = document.getElementsByClassName("p6n-logs-json-key");
+document.addEventListener('click', () => {
+	const elements = document.getElementsByClassName('p6n-logs-json-key');
 	if (elements.length === 0) {
 		return;
 	}
 
-	var requestIdNodes = getLogElementsByName(elements, "requestId: ");
-	for (let i = 0; i < requestIdNodes.length; i++) {
-		var requestIdNode = requestIdNodes[i];
-		var requestIdParentNode = requestIdNode.parentNode;
+	const requestIdNodes = getLogElementsByName(elements, 'requestId: ');
+	for (const requestIdNode of requestIdNodes) {
+		const requestIdParentNode = requestIdNode.parentNode;
 
-		if (requestIdParentNode.getElementsByTagName("a").length != 0) {
+		if (requestIdParentNode.getElementsByTagName('a').length !== 0) {
 			continue;
 		}
 
-		var a = document.createElement('a');
-		var linkText = document.createTextNode("Link to here");
-		a.appendChild(linkText);
-		a.title = "copy";
-		a.style.marginLeft = "10px";
-		requestIdParentNode.appendChild(a);
+		const anchor = document.createElement('a');
+		const linkText = document.createTextNode('Link to here');
+		anchor.appendChild(linkText);
+		anchor.title = 'copy';
+		anchor.style.marginLeft = '10px';
+		requestIdParentNode.appendChild(anchor);
 
-		var requestIdValue = requestIdParentNode.getElementsByClassName("p6n-logs-json-value")[0].innerText;
+		let requestIdValue = requestIdParentNode.getElementsByClassName('p6n-logs-json-value')[0].innerText;
 		requestIdValue = requestIdValue.substring(1, requestIdValue.length - 1);
 
-		var url = window.location.href;
-		url = updateQueryStringParameter(url, "filters", "protoPayload.requestId:" + requestIdValue);
+		let url = window.location.href;
+		url = updateQueryStringParameter(url, 'filters', 'protoPayload.requestId:' + requestIdValue);
 
-		var endTime = getLogElementsByName(requestIdParentNode.parentNode.parentNode.getElementsByClassName("p6n-logs-json-key"), "endTime: ")[0].parentNode.getElementsByClassName("p6n-logs-json-value")[0].innerText;
+		let endTime = getLogElementsByName(requestIdParentNode.parentNode.parentNode.getElementsByClassName('p6n-logs-json-key'), 'endTime: ')[0].parentNode.getElementsByClassName('p6n-logs-json-value')[0].innerText;
 		endTime = endTime.substring(1, endTime.length - 2);
-		endTime += "000Z";
+		endTime += '000Z';
 
-		url = updateQueryStringParameter(url, "timestamp", endTime);
+		url = updateQueryStringParameter(url, 'timestamp', endTime);
 
-		a.href = url;
+		anchor.href = url;
 	}
 });
 
 function getLogElementsByName(elements, name) {
-	return Array.prototype.filter.call(elements, function (testElement) {
-		return testElement.innerText === name;
-	});
+	return Array.prototype.filter.call(elements, testElement => testElement.innerText === name);
 }
-
 
 function updateQueryStringParameter(uri, key, value) {
-	var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
-	var separator = uri.indexOf('?') !== -1 ? "&" : "?";
+	const re = new RegExp('([?&])' + key + '=.*?(&|$)', 'i');
+	const separator = uri.indexOf('?') !== -1 ? '&' : '?';
 	if (uri.match(re)) {
-		return uri.replace(re, '$1' + key + "=" + value + '$2');
-	}
-	else {
-		return uri + separator + key + "=" + value;
+		return uri.replace(re, '$1' + key + '=' + value + '$2');
+	} else {
+		return uri + separator + key + '=' + value;
 	}
 }
-
--- src/bigquery_statistics.ts
+++ src/bigquery_statistics.ts
@@ -1,12 +1,12 @@
-function getAuthToken(resolve: (string) => void) {
-    chrome.runtime.sendMessage({subject : "authToken"}, function(response) {
-		console.log("Response: " + response);
-		if(response === undefined) {
-			console.log(chrome.runtime.lastError)
+function getAuthToken(resolve: (x: string) => void) {
+	chrome.runtime.sendMessage({ subject: 'authToken' }, response => {
+		console.log('Response: ' + response);
+		if (!response) {
+			console.log(chrome.runtime.lastError);
 		} else {
-    	    console.log("Token aquired: " + response.authToken);
-            return resolve(response.authToken);
-        }
+			console.log('Token aquired: ' + response.authToken);
+			return resolve(response.authToken);
+		}
 	});
 }
 
@@ -15,40 +15,37 @@ chrome.runtime.sendMessage({ subject: 'authToken' }, response => {
 		console.error(chrome.runtime.lastError);
 	} else {
 
-		console.log("Dataset.......... ")
-		var url = document.location.href;
+		console.log('Dataset.......... ');
+		const url = document.location.href;
 
-		var projectAndDataset = url.substr(url.lastIndexOf('/') + 1).split(':');
+		const projectAndDataset = url.substr(url.lastIndexOf('/') + 1).split(':');
 
-		var xhr = new XMLHttpRequest();
-		var projectId = projectAndDataset[0];
-		var datasetId = projectAndDataset[1];
+		const xhr = new XMLHttpRequest();
+		const projectId = projectAndDataset[0];
+		const datasetId = projectAndDataset[1];
 
-		console.log("Project: " + projectId);
-		console.log("Dataset: " + datasetId);
+		console.log('Project: ' + projectId);
+		console.log('Dataset: ' + datasetId);
 
-		xhr.onreadystatechange = function () {
-			if (xhr.readyState == 4) {
+		xhr.onreadystatechange = () => {
+			if (xhr.readyState === 4) {
 				console.log(xhr.responseText);
-				var newRow = document.querySelectorAll(".dataset-view tbody tr")[1].cloneNode(true)
-				newRow.querySelectorAll("td")[0].innerHTML = "Data size";
-				newRow.querySelectorAll("td")[1].innerHTML = parseFloat(JSON.parse(xhr.responseText)['rows'][0]['f'][0]['v'], 10)/1024/1024/1024 + " GB";
-				document.querySelector(".dataset-view tbody").appendChild(newRow);
-				
+				const newRow = document.querySelectorAll('.dataset-view tbody tr')[1].cloneNode(true);
+				newRow.querySelectorAll('td')[0].innerHTML = 'Data size';
+				newRow.querySelectorAll('td')[1].innerHTML = parseFloat(JSON.parse(xhr.responseText).rows[0].f[0].v, 10) / 1024 / 1024 / 1024 + ' GB';
+				document.querySelector('.dataset-view tbody').appendChild(newRow);
+
 			}
-		}
-		xhr.open("POST", 'https://www.googleapis.com/bigquery/v2/projects/' + projectId + '/queries', true);
+		};
+		xhr.open('POST', 'https://www.googleapis.com/bigquery/v2/projects/' + projectId + '/queries', true);
 		xhr.setRequestHeader('Authorization', 'Bearer ' + response.authToken);
 		xhr.setRequestHeader('content-type', 'application/json');
-		:
 
 		xhr.send(JSON.stringify({
-			"query": "select sum(size_bytes) as size from [" + projectId + ":" + datasetId + ".__TABLES__]",
-			"kind": "bigquery#queryRequest",
-			"useLegacySql": true
+			query: 'select sum(size_bytes) as size from [' + projectId + ':' + datasetId + '.__TABLES__]',
+			kind: 'bigquery#queryRequest',
+			useLegacySql: true,
 		}));
 
-
 	}
-	});
-
+});
