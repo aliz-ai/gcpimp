0e679c9e4ea8440f946ff196abb812bee3e6bacc
Gergely Sipos <gergely.sipos@doctusoft.com>
2018-07-03 19:46:42 +0200

    removed old code

--- src/old/prod-color.ts
+++
@@ -1,6 +0,0 @@
-import { waitFor } from './utils';
-
-const pageSystemBar = () => document.querySelector('.p6n-system-bar') as HTMLElement;
-
-waitFor(() => !!pageSystemBar())
-	.then(() => pageSystemBar().style.backgroundColor = 'rgb(175, 36, 36)');
--- src/old/utils.ts
+++
@@ -1,9 +0,0 @@
-export function waitFor(isPresent: () => boolean) {
-	return Promise.race([...Array(10)]
-		.map((u, idx) => idx * 100)
-		.map(time => new Promise(resolve =>
-			setTimeout(() => isPresent()
-				? resolve()
-				: undefined,
-				time))));
-}
